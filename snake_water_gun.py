554a0c6413e2304bba8d96e457543348104c3a31
SHIVA SURYA S <shivasurya470@gmail.com>
2026-01-22 20:29:36 +0530

    Implement Snake Water Gun game in Python
    Implemented the classic Snake, Water, Gun game using Python.
    Includes user input handling, random computer choice, and win–loss logic.
    Demonstrates basic control flow and conditionals.

---
+++ snake_water_gun.py
@@ -0,0 +1,32 @@
+import random
+
+computer = random.choice([-1, 0, 1])
+youstr = input("enter your choice: ")
+youDict = {"s" : 1, "w" : -1, "g" : 0}
+reverseDict = {1 : "snake", -1 : "water", 0 : "gun"}
+
+you = youDict[youstr]
+
+print(f"You chose {reverseDict[you]} . computer chose {reverseDict[computer]}")
+
+if(computer == you):
+    print("It's a draw!!")
+
+else:
+    if(computer == -1 and you == 0):
+        print("computer wins!")
+
+    elif(computer == -1 and you == 1):
+        print("you win!!")
+
+    elif(computer == 1 and you == -1):
+        print("computer win!")
+
+    elif(computer == 1 and you == 0):
+        print("you win!!")
+
+    elif(computer == 0 and you == 1):
+        print("computer win!")
+
+    elif(computer == 0 and you == -1):
+        print("you win!!")
